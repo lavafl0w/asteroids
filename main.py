0c271c9d92b2ddfecc883b084348e050a655ebf0
lavafl0w <lavafl0w@hidden.aleeas.com>
2026-07-10 14:56:25 +0100

    implemented grouping of objects

--- main.py
+++ main.py
@@ -4,20 +4,32 @@ from logger import log_state
 import player
 
 
-def main():
+def main() -> None:
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-    pygame.init() # Create pygame instance
+    # Create Pygame Instance
+    pygame.init()
 
-    py_clock = pygame.time.Clock() # Create FPS clock
+    # Internal Components
+    py_clock = pygame.time.Clock() # FPS clock
     dt = 0.0 # Delta time - Change in time
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # Set screen size as defined in constants.py
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # Set screen size from constants.py
+    
+    # Group Creation
+    updatable = pygame.sprite.Group() # Empty group of updatable objects
+    drawable = pygame.sprite.Group() # Empty group of drawbale objects
+    
+    # Populating Groups
+    player.Player.containers = (updatable, drawable) # Player class -> updatable and drawable groups
 
+    # Object Creation
     player1 = player.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) # Create player object
 
+
+    # Game Loop
     while True:
 
         log_state() # Start logger
@@ -28,12 +40,13 @@ def main():
                 return
 
         screen.fill("black") # Set background
+        
+        for item in drawable:
+            item.draw(screen) # Draw everything in drawable group
+        
+        updatable.update(dt) # Update any movement in updatable group
 
-        player1.draw(screen) # Draw player
-        player1.update(dt)        
-
-        pygame.display.flip() # Refreshes display
-
+        pygame.display.flip() # Refresh display
         dt = py_clock.tick(60) / 1000 # Ticks at 60 FPS (devision of 1000 is for milliseconds)
 
 if __name__ == "__main__":
--- player.py
+++ player.py
@@ -21,6 +21,14 @@ class Player(circleshape.CircleShape):
     def draw(self, screen: pygame.Surface):
         pygame.draw.polygon(screen, "white", self.triangle(), constants.LINE_WIDTH)
 
+    # Move back and forward
+    def move(self, dt: float) -> None:
+        unit_vector = pygame.math.Vector2(0,1) # Creates a unit vector of length 1
+        rotated_vector = unit_vector.rotate(self.rotation) # Rotates vector in same direction of player
+        speed_vector = rotated_vector * constants.PLAYER_SPEED * dt # Extends the length of the vector by how much the player should move in frame
+        self.position += speed_vector # Makes this the new position
+
+
     # Rotates player sprite
     def rotate(self, dt: float) -> None:
         self.rotation += constants.PLAYER_TURN_SPEED * dt
@@ -29,7 +37,11 @@ class Player(circleshape.CircleShape):
     def update(self, dt: float) -> None:
         keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_a]: # Rotates left
+        if keys[pygame.K_w]: # Move forward - Key: W
+            self.move(dt)
+        if keys[pygame.K_a]: # Rotates left - Key: A
             self.rotate(-dt)
-        if keys[pygame.K_d]: # Rotates right
+        if keys[pygame.K_s]: # Move backward - Key : S
+            self.move(-dt)
+        if keys[pygame.K_d]: # Rotates right - Key: D
             self.rotate(dt)
