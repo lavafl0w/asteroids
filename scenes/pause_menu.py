d30086f9daff3804676d59dfaaf877fee4d0039c
lavafl0w <lavafl0w@hidden.aleeas.com>
2026-08-09 20:16:58 +0100

    Positioned pause screen

--- scenes/pause_menu.py
+++ scenes/pause_menu.py
@@ -6,9 +6,9 @@ import random
 
 class PauseMenu:
     def __init__(self, screen:pygame.Surface) -> None:
-        self.pause_screen = screen.copy()
-        self.pause_screen.set_alpha(75)
-        self.pause_screen.fill((8, 12, 22))
+        self.pause_screen_background = screen.copy()
+        self.pause_screen_background.set_alpha(75)
+        self.pause_screen_background.fill((8, 12, 22))
         self.title_font = pygame.font.Font("assets/fonts/orbitron/Orbitron-Bold.ttf", 60)
         self.subtitle_font = pygame.font.Font("assets/fonts/oxanium/Oxanium-Regular.ttf", 26)
         self.text_font = pygame.font.Font("assets/fonts/exo/Exo-Regular.ttf", 25)
@@ -25,29 +25,42 @@ class PauseMenu:
     
     def draw(self, screen: pygame.Surface) -> None:
         screen_rect = screen.get_rect()
-        screen.blit(self.pause_screen, (0, 0))
+        screen.blit(self.pause_screen_background, (0, 0))
         
+        # Get each surface
         pause_title_surface = self.title_font.render("YOU HAVE PAUSED TIME! OMG!!", 1, "white")
-        pause_title_rect = pause_title_surface.get_rect()
-        pause_title_rect.center = (screen_rect.centerx, 80)
-        screen.blit(pause_title_surface, pause_title_rect)
-        
-        pause_subtitle_surface = self.subtitle_font.render("Can't believe you figured out how to do something like that! :o", 1, "white")
-        pause_subtitle_rect = pause_subtitle_surface.get_rect()
-        pause_subtitle_rect.centerx = screen_rect.centerx
-        pause_subtitle_rect.top = pause_title_rect.bottom + 10
-        screen.blit(pause_subtitle_surface, pause_subtitle_rect)
-        
+        pause_subtitle_surface = self.subtitle_font.render("Can't believe you figured out how to do something like that! :o",1, "white")
         pause_sub_subtitle_surface = self.subtitle_font.render("You're amazing! :o", 1, "white")
-        pause_sub_subtitle_rect = pause_sub_subtitle_surface.get_rect()
-        pause_sub_subtitle_rect.centerx = screen_rect.centerx
-        pause_sub_subtitle_rect.top = pause_subtitle_rect.bottom
-        screen.blit(pause_sub_subtitle_surface, pause_sub_subtitle_rect)
-                
         pause_text_surface = self.text_font.render("Press ESC to continue.", 1, "white")
+        
+        # Find the rects of all the surfaces
+        pause_title_rect = pause_title_surface.get_rect()
+        pause_subtitle_rect = pause_subtitle_surface.get_rect()
+        pause_sub_subtitle_rect = pause_sub_subtitle_surface.get_rect()
         pause_text_rect = pause_text_surface.get_rect()
-        pause_text_rect.centerx = screen_rect.centerx
+        
+        # Get the total height of all the rects (and later also buffers) to vertically centre on screen
+        total_base_rect_height = (pause_title_rect.height + pause_subtitle_rect.height +
+                             pause_sub_subtitle_rect.height + pause_text_rect.height)
+        total_rect_height = total_base_rect_height + 60 # This adds in the 10 / 50 pixel buffers
+        
+        # Position rects relative to title rect (which starts at the top of the total height)
+        pause_title_rect.top = screen_rect.centery - total_rect_height // 2
+        pause_title_rect.centerx = screen_rect.centerx
+        
+        pause_subtitle_rect.top = pause_title_rect.bottom + 10
+        pause_subtitle_rect.centerx = screen_rect.centerx
+        
+        pause_sub_subtitle_rect.top = pause_subtitle_rect.bottom
+        pause_sub_subtitle_rect.centerx = screen_rect.centerx
+                
         pause_text_rect.top = pause_sub_subtitle_rect.bottom + 50
+        pause_text_rect.centerx = screen_rect.centerx
+        
+        # Blit them all on screen
+        screen.blit(pause_title_surface, pause_title_rect)
+        screen.blit(pause_subtitle_surface, pause_subtitle_rect)
+        screen.blit(pause_sub_subtitle_surface, pause_sub_subtitle_rect)
         screen.blit(pause_text_surface, pause_text_rect)
     
     def update(self, dt:float) -> None:
